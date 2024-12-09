79f18d171769a09edcea66665e64abbb482b0366
KarynaPosokhina <r1024664@student.thomasmore.be>
2024-12-09 20:07:03 +0100

    First names exercise 1, text and files

---
+++ text_files_exercises/first_names_txt_exercise1.py
@@ -0,0 +1,22 @@
+with open("first_names.txt", "r") as file:
+    for line in file:
+        print(line.strip())
+
+names = 0
+with open("first_names.txt", "r") as file:
+    for line in file:
+        names += 1
+
+print(f"There are {names} first names in the file")
+
+z_names = 0
+with open("first_names.txt", "r") as file:
+    for line in file:
+        name = line.strip()
+        if 'z' in name:
+            print(name)
+            z_names += 1
+
+print(f"{z_names} of them contain the letter 'z'")
+
+
