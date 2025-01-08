67d62b6e2d28055a1d64d705c46313570a3f6bbf
KarynaPosokhina <r1024664@student.thomasmore.be>
2025-01-08 13:44:33 +0100

    Plants exersice 2c, json and yaml

--- json_yaml_ex/plants_ex2.py
+++ json_yaml_ex/plants_ex2.py
@@ -7,12 +7,18 @@ datadict = data["catalog"]
 counter = 1
 
 for plant in datadict:
-    print(f"Plant number {counter}:")
-    print("\t", "common " + plant.get("common"))
-    print("\t", "botanical " + plant.get("botanical"))
-    print("\t", "zone " + str(plant.get("zone")))
-    print("\t", "light " + plant.get("light"))
-    print("\t", "price " + str(plant.get("price")))
-    print("\t", "availability " + plant.get("availability"))
+    if plant.get("light") == "SUN":
+        print(f'Plant number {counter}: ')
+        print("\t" "Name: " + plant.get("common"))
+        print("\t" "Light: " + plant.get("light"))
+        print("\t" "Price: " + str(plant.get("price")))
 
-    counter += 1
+        # print(f"Plant number {counter}:")
+        # print("\t", "common " + plant.get("common"))
+        # print("\t", "botanical " + plant.get("botanical"))
+        # print("\t", "zone " + str(plant.get("zone")))
+        # print("\t", "light " + plant.get("light"))
+        # print("\t", "price " + str(plant.get("price")))
+        # print("\t", "availability " + plant.get("availability"))
+
+        counter += 1
